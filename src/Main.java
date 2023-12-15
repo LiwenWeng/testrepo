2db9f9504fc49ae9ba0ac4912d481285af23e74f
Markus <markus.skakun@gmail.com>
2023-12-15 10:42:14 -0500

    3rd partner code

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Wassup guys");
         System.out.println("yooo");
+        System.out.println("sick");
     }
 }
